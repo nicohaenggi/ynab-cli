9f6aebb40c883047e61a74488d3a224acd24e297
Pascal Küng <pascalkueng@protonmail.com>
2023-02-11 16:59:05 +0100

    feat: implement oauth poc

--- src/commands/auth/login.ts
+++ src/commands/auth/login.ts
@@ -1,4 +1,7 @@
 import { Args, Command, Flags } from '@oclif/core';
+import express from 'express';
+import open from 'open';
+import { fetch } from 'undici';
 
 export default class AuthLoginCommand extends Command {
   static override description = 'LOGIN command';
@@ -13,6 +16,32 @@ export default class AuthLoginCommand extends Command {
 
   async run(): Promise<void> {
     const { args, flags } = await this.parse(AuthLoginCommand);
+
+    const app = express();
+    let resolve: any;
+    const p = new Promise((_resolve) => {
+      resolve = _resolve;
+    });
+    app.get('/oauth', function (req, res) {
+      resolve(req.query['code']);
+      res.end('');
+    });
+    app.listen(80);
+
+    open(
+      `https://app.youneedabudget.com/oauth/authorize?client_id=eGgeAUn2fDv-kfv54XAOZfux0NX6KGxIz9qD9HhM9ek&redirect_uri=http%3A%2F%2Flocalhost%2Foauth&response_type=code`,
+    );
+
+    const code = await p;
+
+    const response = await fetch(
+      `https://app.youneedabudget.com/oauth/token?client_id=eGgeAUn2fDv-kfv54XAOZfux0NX6KGxIz9qD9HhM9ek&client_secret=xxxxxxxx&redirect_uri=http%3A%2F%2Flocalhost%2Foauth&grant_type=authorization_code&code=${code}`,
+      { method: 'POST' },
+    );
+
+    const text = await response.text();
+    const token = JSON.parse(text);
+
     this.log(`hello from 'AUTH LOGIN'`, args, flags);
   }
 }
